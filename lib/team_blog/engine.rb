3c365c4ec9093ffe3c6b97ac0125a4bdfe970af5
jasl <jasl9187@hotmail.com>
2013-01-29 01:39:51 +0800

    fix precompile

--- lib/team_blog/engine.rb
+++ lib/team_blog/engine.rb
@@ -5,9 +5,9 @@ module TeamBlog
 
     initializer "TeamBlog precompile hook", :group => :all do |app|
       app.config.assets.precompile += %w(team_blog/application.js
-                                         team_blog/application.css.scss
+                                         team_blog/application.css
                                          team_blog/admin/application.js
-                                         team_blog/admin/application.css.scss)
+                                         team_blog/admin/application.css)
     end
     isolate_namespace TeamBlog
   end
